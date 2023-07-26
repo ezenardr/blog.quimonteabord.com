dc44e784c50e29a4488f906a5a7731abe81ada87
ezenardr <ezenardr.dev@gmail.com>
2023-07-26 12:10:12 -0400

    Removed env from nextjs config and removed shadow db url from schema

--- next.config.js
+++ next.config.js
@@ -12,10 +12,5 @@ const nextConfig = {
             },
         ],
     },
-    env: {
-        NEXT_PUBLIC_SUPABASE_URL: 'https://pivnsfqramdzzbitmnie.supabase.co',
-        SUPABASE_SERVICE_ROLE_KEY:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpdm5zZnFyYW1kenpiaXRtbmllIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NTE5MDk3NSwiZXhwIjoyMDAwNzY2OTc1fQ.tYknG_OJifL32N-2Rk2vl54eslDqyv0FeXzIn1-dJNc',
-    },
 };
 module.exports = nextConfig;
