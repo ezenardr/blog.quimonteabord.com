392dfceee264cc2dc54ce474b666d88de42f45a8
ezenardr <ezenardr.dev@gmail.com>
2023-10-27 14:25:41 -0400

    Modified sitemap.ts; deleted route for getPosts, => build failed

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,29 @@
 import { MetadataRoute } from 'next';
+import db from '@/db/dbConnect';
+import * as schema from '@/db/schema';
 
 type ArticleProps = {
-    updatedAt: Date | null;
+    image: string;
+    createdAt: Date | null;
+    updatedAt: any;
     post_id: string;
+    title: string;
+    body: string;
+    author_name: string;
+    author_id: string;
 };
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseurl = 'https://blog.quimonteabord.com';
-    const posts = await fetch('http://localhost:3000/api/getPosts').then(
-        async (res) => {
-            return await res.json();
-        }
-    );
-    console.log(posts);
-    const postUrls = await posts.map((post: ArticleProps) => ({
+    const posts = await db.select().from(schema.post);
+
+    const postUrls = posts.map((post: ArticleProps) => ({
         url: `${baseurl}/post/${post.post_id}`,
         lastModified: post.updatedAt,
-        changeFrequency: 'hourly',
     }));
     return [
         {
             url: `${baseurl}`,
             lastModified: new Date(),
-            changeFrequency: 'hourly',
         },
         ...postUrls,
     ];
