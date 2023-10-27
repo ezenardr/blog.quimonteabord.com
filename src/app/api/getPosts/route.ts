911d910dbfc17b3d6c2a125e10ee2a17f1e9feec
ezenardr <ezenardr.dev@gmail.com>
2023-10-27 13:05:54 -0400

    Modified sitemap.ts and getPosts route, failed generating sitemap in build, work in dev now

--- src/app/api/getPosts/route.ts
+++ src/app/api/getPosts/route.ts
@@ -4,16 +4,16 @@ import * as schema from '@/db/schema';
 
 export async function GET(request: NextRequest) {
     const article = await db.select().from(schema.post);
+    if (!article)
+        return NextResponse.json({
+            success: false,
+            message: 'Failed to fetch Data',
+        });
     const newArticleObj = article.map((post) => {
         return {
             post_id: post.post_id,
             updatedAt: post.updatedAt,
         };
     });
-    if (!article)
-        return NextResponse.json({
-            success: false,
-            message: 'Failed to fetch Data',
-        });
     return NextResponse.json(newArticleObj);
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,27 @@
+import { MetadataRoute } from 'next';
+
 type ArticleProps = {
     updatedAt: Date | null;
     post_id: string;
 };
-export default async function sitemap() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseurl = 'https://blog.quimonteabord.com';
     const posts = await fetch('http://localhost:3000/api/getPosts').then(
-        (res) => res.json()
+        async (res) => {
+            return await res.json();
+        }
     );
-    const postUrls = await posts.map((post: ArticleProps) => {
-        return {
-            url: `${baseurl}/post/${post.post_id}`,
-            lastModified: post.updatedAt,
-        };
-    });
+    console.log(posts);
+    const postUrls = await posts.map((post: ArticleProps) => ({
+        url: `${baseurl}/post/${post.post_id}`,
+        lastModified: post.updatedAt,
+        changeFrequency: 'hourly',
+    }));
     return [
         {
             url: `${baseurl}`,
             lastModified: new Date(),
+            changeFrequency: 'hourly',
         },
         ...postUrls,
     ];
