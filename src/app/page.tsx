40f605c4211ce39ca3a4bab5d5e43f6f7c829a93
ezenardr <ezenardr.dev@gmail.com>
2023-10-28 14:40:08 -0400

    Fixed : padding in header text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Card from '@/components/card/Card';
 import Style from './page.module.scss';
-import Navigation from '@/components/nav/Navigation';
 import SignUp from '@/components/signup/SignUp';
 import db from '@/db/dbConnect';
 import { Suspense } from 'react';
