6f03963151a0bf5e017c2fb16f3a866205793c72
ezenardr <ezenardr.dev@gmail.com>
2023-10-28 12:33:57 -0400

    Fixed: sitemap link in robot.ts

--- src/app/robot.ts
+++ src/app/robot.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/admin/',
         },
-        sitemap: 'https://quimonteabord.com/sitemap.xml',
+        sitemap: 'https://blog.quimonteabord.com/sitemap.xml',
     };
 }
